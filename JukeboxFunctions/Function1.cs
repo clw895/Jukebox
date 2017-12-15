a74a09688dc200212f129adb2fe7b93c083e3987
Corey Weathers <clw895@msn.com>
2017-12-15 12:11:46 -0800

    Converts project to V2 of Azure Functions runtime

--- JukeboxFunctions/Function1.cs
+++ JukeboxFunctions/Function1.cs
@@ -1,35 +1,26 @@
 using Twilio.TwiML;
 using Twilio.TwiML.Messaging;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JukeboxFunctions
 {
     public static class Function1
     {
         [FunctionName("ReplyToJukeboxRequest")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            return GetTwilioResponse();
-        }
-
-        public static HttpResponseMessage GetTwilioResponse()
-        {
             var twilioResponse = new MessagingResponse();
             twilioResponse.Append(new Message("Thanks for sending your request. We will reply to your request shortly"));
 
-            var httpResponseMessage = new HttpResponseMessage();
-            httpResponseMessage.Content = new StringContent(twilioResponse.ToString());
-            httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/xml");
-
-            return httpResponseMessage;
+            return new ContentResult{ Content = twilioResponse.ToString(), ContentType = "application/xml" };
         }
     }
 }
